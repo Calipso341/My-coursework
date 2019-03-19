9c08f4bfecf5f8a412804f664575c3b4a49119c9
Calipso341 <tatarintsev.nik@gmail.com>
2019-03-19 21:39:40 +0200

    Just tried to make a Login function

--- Accounting of the enterprice/Accounting of the enterprice/Source.cpp
+++ Accounting of the enterprice/Accounting of the enterprice/Source.cpp
@@ -2,19 +2,38 @@
 #include<Windows.h>
 #include<string>
 
-void LogIn(char name,int pasw) {
+void LogIn(string login, string passwrd, string log1, string passwrd1) {
 
-	cout << "\n\tEnter your name: ";
-	cin >> name;
+	for (int i = 0; i < 3; i++)
+	{
+		cout << "\n\tAttempt to login number [ " << i + 1 << " ] " << "\n\tEnter your login: ";
+		cin >> login;
+		if (login == log1) {
+			cout << "\n\tLogin is correct! Enter the password pleas: ";
+		}
+		cin >> passwrd;
+		if (passwrd == passwrd1) {
+			cout << "\n\tPassrword is correct!\n\tLogining is successful!";
+		}
+		else cout << "\n\tLoginin failed! Maybe login or password is wrong!" << endl;
+	}
+
 }
 
 using namespace std;
 
 int main() {
 
+	string login = "Admin";
+	string passwrd = "Admin123";
+	string log1;
+	string passwrd1;
+
 	cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\tLoading..." << endl;
 	Sleep(1000);
 	system("cls");
+
+	LogIn(login, passwrd, log1, passwrd1);
 
 	system("pause");
 	return 0;
