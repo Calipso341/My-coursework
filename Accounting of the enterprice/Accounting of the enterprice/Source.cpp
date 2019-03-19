dc849517da8bf374ee7b1967866d1c4ac0c3c68d
Calipso341 <tatarintsev.nik@gmail.com>
2019-03-19 21:00:39 +0200

    Finally started my courcework

---
+++ Accounting of the enterprice/Accounting of the enterprice/Source.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<Windows.h>
+#include<string>
+
+void LogIn(char name,int pasw) {
+
+	cout << "\n\tEnter your name: ";
+	cin >> name;
+}
+
+using namespace std;
+
+int main() {
+
+	cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\tLoading..." << endl;
+	Sleep(1000);
+	system("cls");
+
+	system("pause");
+	return 0;
+}
