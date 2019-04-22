9830c7f75be15410d893775c0e5061d349023d08
Calipso341 <tatarintsev.nik@gmail.com>
2019-04-22 13:39:45 +0300

    fix

--- Accounting of the enterprice/Accounting of the enterprice/Source.cpp
+++ Accounting of the enterprice/Accounting of the enterprice/Source.cpp
@@ -1,112 +1,255 @@
 #include<iostream>
 #include<Windows.h>
-#include<fstream>
 #include<string>
+#include<fstream>
 
 using namespace std;
 
-struct Info {
-
-	string name;
-	string surname;
-	short int age;
-	string discription;
-	string post;
-	short int workDays;
-	short int weekends;
-
-	void ShowInfo() {
-
-		cout << "\n\n\tName: " << name << "\n\n\tSurname: " << surname << "\n\n\tAge: " << age << "\n\n\tDiscription: " << discription << "\n\n\tPost: " << post << "\n\n\tNumber of work days: " << workDays << "\n\n\tNumber of weekends: " << weekends << endl;
-	};
-
-	string SaveFile() {
-
-		string txt;
-
-		txt = "\n\tName: " + name + "\n\tSurname: " + surname + "\n\tAge: " + to_string(age) + "\n\tDiscription: " + discription + "\n\tPost: " + post + "\n\tNamber of work days: " + to_string(workDays) + "\n\tNumber of weekends: " + to_string(weekends);
-
-		return txt;
-	}
+struct WorkerInfo
+{
+	string name, surname, discription, post;
+	long long int phone;
+	int age, workdays, weekends;
 };
 
-void FillInfo() {
+void FillCash()
+{
+	int day, month, year, profit, sallary, lesion;
 
+	string Money = "Money.txt";
+
+	ofstream Cash;
+
+	Cash.open(Money, ofstream::app);
+
+	cout << "\n\n\tEnter day of month you want to fill info about: ";
+	cin >> day;
+	cout << "\n\n\tEnter month of year you want to fill info about: ";
+	cin >> month;
+	cout << "\n\n\tEnter year you want to fill info about: ";
+	cin >> year;
+	cout << "\n\n\tEnter amount of profit: ";
+	cin >> profit;
+	cout << "\n\n\tEnter amount of sallary: ";
+	cin >> sallary;
+	cout << "\n\n\tEnter amount of lesion: ";
+	cin >> lesion;
+	Cash << "\n\n\t::::::::::::::::::::::::::::::::::::::" << "\n\n\tDate: " << day << "." << month << "." << year << "\n\n\tProfit: " << profit << "\n\n\tSallary: " << sallary << "\n\n\tLesion: " << lesion;
+	Cash.close();
+}
+
+void FillInfo()
+{
 	int Size = 0;
 
-	cout << "\n\tEnter the number of employees you will fill information about: ";
+	cout << "\n\n\tEneter number of workers: ";
 
 	cin >> Size;
 
-	Info *employee = new Info[Size];
+	WorkerInfo *worker = new WorkerInfo[Size];
+
+	string file = "Info.txt";
+
+	ofstream info;
+
+	info.open(file, ofstream::app);
 
 	for (int i = 0; i < Size; i++)
 	{
-		cout << "\n\tEnter name of employee: ";
-		cin >> employee[i].name;
-		cout << "\n\tEnter surname of employee: ";
-		cin >> employee[i].surname;
-		cout << "\n\tEnter age of employee: ";
-		cin >> employee[i].age;
-		cout << "\n\tEnter discription about employee: ";
-		cin >> employee[i].discription;
-		cout << "\n\tEnter post of employee: ";
-		cin >> employee[i].post;
-		cout << "\n\tEnter number of work days of employee: ";
-		cin >> employee[i].workDays;
-		cout << "\n\tEnter weekends of employee: ";
-		cin >> employee[i].weekends;
-		cout << "\n\t============================================" << endl << endl;
+		cout << "\n\n\tEnter the name: ";
+		cin >> worker[i].name;
+		cout << "\n\n\tEnter tne surname: ";
+		cin >> worker[i].surname;
+		cout << "\n\n\tEnter the discription: ";
+		cin.ignore();
+		getline(cin, worker[i].discription);
+		cout << "\n\n\tEnter the post: ";
+		cin >> worker[i].post;
+		cout << "\n\n\tEnter the phone number: ";
+		cin >> worker[i].phone;
+		cout << "\n\n\tEnter the age: ";
+		cin >> worker[i].age;
+		cout << "\n\n\tEnter the number of work days: ";
+		cin >> worker[i].workdays;
+		cout << "\n\n\tEnter the number of weekends: ";
+		cin >> worker[i].weekends;
+		cout << "\n\n\t";
+		info << "\n\n\t::::::::::::::::::::::::::::::::::::::\n\n\tName: " << worker[i].name << "\n\n\tSurname: " << worker[i].surname << "\n\n\tDiskription: " << worker[i].discription << "\n\n\tPost: " << worker[i].post << "\n\n\tPhone: " << worker[i].phone << "\n\n\tAge: " << worker[i].age << "\n\n\tWork days: " << worker[i].workdays << "\n\n\tWeekends: " << worker[i].weekends;
 	}
-	ofstream file;
+	info.close();
+}
 
-	file.open("I.E.txt", ofstream::app);
+void GetMoneyInfo()
+{
+	string Money = "Money.txt";
 
-	for (int i = 0; i < Size; i++)
+	ifstream Cash;
+
+	Cash.open(Money);
+
+	if (!Cash.is_open())
 	{
-		file << employee[i].SaveFile();
-		file.close();
+		cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\tFile does not exit! Creat one before read it!";
+		Sleep(3000);
+		system("cls");
+		cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\tLoading...";
+		Sleep(1000);
+		system("cls");
+	}
+	else
+	{
+		char ch2;
+
+		while (Cash.get(ch2))
+		{
+			cout << ch2;
+		}
+	}
+}
+void GetInfo()
+{
+	string file = "Info.txt";
+
+	ifstream info;
+
+	info.open(file);
+
+	if (!info.is_open())
+	{
+		cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\tFile does not exit! Creat one before read it!";
+		Sleep(3000);
+		system("cls");
+		cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\tLoading...";
+		Sleep(1000);
+		system("cls");
+	}
+	else
+	{
+		char ch;
+
+		while (info.get(ch))
+		{
+			cout << ch;
+		}
 	}
 }
 
 void Menu()
 {
-	int choice = 0;
-	cout << "\n\t\t\t\t\t\t_______________________________________" << endl;
-	cout << "\n\t\t\t\t\t\t  Accounting of the enterprise (0.1)." << endl;
-	cout << "\n\t________________________________________________________________________________________________________" << endl;
-	cout << "\n\tTo fill information about employee, PRESS 1. " << "\n\n\tTo add new employee, PRESS 2. " << "\n\n\tTo delete an employee and info about him, PRESS 3. " << "\n\n\tTo see information about all employees, PRESS 4. " << "\n\n\tTo see information about direct employee, PRESS 5. " << "\n\n\tTo exit, PRESS 0. " << endl << endl;
-	cout << "\t________________________________________________________________________________________________________" << endl;
-	cout << "\n\tYour choice: ";
-	cin >> choice;
-	if (int choice = 4) {
-		system("cls");
-		cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\tLoading...";
-		Sleep(1000);
-		system("cls");
-		Info info;
-		info.ShowInfo();
-	}
-	if (int choice = 1) {
+	int MenuChoise = 0;
 
-		system("cls");
-		cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\tLoading...";
-		Sleep(1000);
+	cout << "\n\t\t\t\t\t\tVersion 0.1.0\t\t\t\t\tCreated by Calipso341" << "\n\n\tTo fill information about new employee, PRESS [1]. " << "\n\n\tTo fill information about money, PRESS [2]." << "\n\n\tTo delete an employee and info about him, PRESS [3] - (Yet not working, still in progress). " << "\n\n\tTo see information about all employees, PRESS [4]. " << "\n\n\tTo see information about money, PRESS [5]. " << "\n\n\tTo see information about direct employee, PRESS [6] - (Yet not working, still in progress). " << "\n\n\tTo exit, PRESS [0]. " << "\n\n\t\t\t\t\t\tManual\n\n\tWelcome to shortest manual ever!" << "\n\n\tFor moving in menu PRESS [desired] button, then PRESS [enter]. \n\n\tHooray, now you could use my program and move in menu!" << "\n\n\tYour choise: ";
+
+	cin >> MenuChoise;
+
+	if (MenuChoise == 1)
+	{
 		system("cls");
 		FillInfo();
+		system("cls");
+		cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\t\tSaving info in file...";
+		Sleep(1000);
+		system("cls");
+		Menu();
+	}
+	else if (MenuChoise == 2)
+	{
+		system("cls");
+		cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\tLoading...";
+		Sleep(1000);
+		system("cls");
+		FillCash();
+		system("cls");
+		cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\t\tSaving info in file...";
+		Sleep(1000);
+		system("cls");
+		Menu();
+	}
+	else if (MenuChoise == 4)
+	{
+		int Choise1 = 0;
 
+		system("cls");
+		cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\tLoading...";
+		Sleep(1000);
+		system("cls");
+		GetInfo();
+		cout << "\n\n\t::::::::::::::::::::::::::::::::::::::\n\n\tTo move back to menu, PRESS [1]" << "\n\n\tYour choise: ";
+		cin >> Choise1;
+
+		if (Choise1 == 1)
+		{
+			system("cls");
+			cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\tLoading...";
+			Sleep(1000);
+			system("cls");
+			Menu();
+		}
+		else
+		{
+			system("cls");
+			cout << "\n\n\tWrong button, you will automatically move to the menu...";
+			Sleep(1000);
+			system("cls");
+			Menu();
+		}
+	}
+	else if (MenuChoise == 5)
+	{
+		int Choise4 = 0;
+
+		system("cls");
+		cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\tLoading...";
+		Sleep(1000);
+		system("cls");
+		GetMoneyInfo();
+		cout << "\n\n\t::::::::::::::::::::::::::::::::::::::\n\n\tTo move back to menu, PRESS [1]" << "\n\n\tYour choise: ";
+		cin >> Choise4;
+
+		if (Choise4 == 1)
+		{
+			system("cls");
+			cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\tLoading...";
+			Sleep(1000);
+			system("cls");
+			Menu();
+		}
+		else
+		{
+			system("cls");
+			cout << "\n\n\tWrong button, you will automatically move to the menu...";
+			Sleep(1000);
+			system("cls");
+			Menu();
+		}
+	}
+	else if (MenuChoise == 0)
+	{
+		system("cls");
+		cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\tBye!";
+		Sleep(1000);
+		exit(0);
+	}
+	else
+	{
+		system("cls");
+		cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\tOpen your eyes and PRESS correct buttons!";
+		Sleep(3000);
+		system("cls");
+		Menu();
 	}
 }
 
-void LogIn(string login, string passwd, string login1, string passwd1)
+void Login(string log, string passwd, string log1, string passwd1)
 {
-	cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\tTo enter the program, please, enter info below." << endl;
+	cout << "\n\tLogin: master.\n\n\tPassword: 1.";
+	cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\tTo enter the program, please, enter information below." << endl;
 	cout << "\n\t\t\t\t\t\tEnter your login: ";
-	cin >> login1;
+	cin >> log1;
 	cout << "\n\t\t\t\t\t\tEnter your password: ";
 	cin >> passwd1;
 
-	if (login1 == login && passwd1 == passwd) {
+	if (log1 == log && passwd1 == passwd) {
 
 		system("cls");
 		cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\tAccess is alowed.";
@@ -121,21 +264,14 @@ void LogIn(string login, string passwd, string login1, string passwd1)
 	}
 }
 
-int main() {
-
-	string login = "1";
+int main()
+{
+	string log = "master";
 	string passwd = "1";
 
-	string login1;
-	string passwd1;
+	string log1, passwd1;
 
-	cout << endl;
-	cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\tWe preapering everything to start, please wait a moment..." << endl;
-	cout << "\n\t\t\t\t\t\tLoading..." << endl;
-	Sleep(1000);
-	system("cls");
-
-	LogIn(login, passwd, login1, passwd1);
+	Login(log, passwd, log1, passwd1);
 
 	system("pause");
 	return 0;
