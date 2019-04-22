8ba158c13d45ae24a62deef90c4c745811bcdfcd
Calipso341 <tatarintsev.nik@gmail.com>
2019-04-22 13:23:52 +0300

    Fix

--- Accounting of the enterprice/Accounting of the enterprice/Source.cpp
+++ Accounting of the enterprice/Accounting of the enterprice/Source.cpp
@@ -1,11 +1,12 @@
 #include<iostream>
 #include<Windows.h>
+#include<fstream>
 #include<string>
 
 using namespace std;
 
-struct Employee {
-public:
+struct Info {
+
 	string name;
 	string surname;
 	short int age;
@@ -13,42 +14,91 @@ public:
 	string post;
 	short int workDays;
 	short int weekends;
-	short int salary;
+
 	void ShowInfo() {
-		int choice = 0;
-		cout << "\n\n\tName: " << name << "\n\n\tSurname: " << surname << "\n\n\tAge: " << age << "\n\n\tDiscription: " << discription << "\n\n\tPost: " << post << "\n\n\tNumber of work days: " << workDays << "\n\n\tNumber of weekends: " << weekends << "\n\n\tSalary: " << salary << " grn" << endl;
+
+		cout << "\n\n\tName: " << name << "\n\n\tSurname: " << surname << "\n\n\tAge: " << age << "\n\n\tDiscription: " << discription << "\n\n\tPost: " << post << "\n\n\tNumber of work days: " << workDays << "\n\n\tNumber of weekends: " << weekends << endl;
 	};
+
+	string SaveFile() {
+
+		string txt;
+
+		txt = "\n\tName: " + name + "\n\tSurname: " + surname + "\n\tAge: " + to_string(age) + "\n\tDiscription: " + discription + "\n\tPost: " + post + "\n\tNamber of work days: " + to_string(workDays) + "\n\tNumber of weekends: " + to_string(weekends);
+
+		return txt;
+	}
 };
 
-void Menu(Employee Bill)
+void FillInfo() {
+
+	int Size = 0;
+
+	cout << "\n\tEnter the number of employees you will fill information about: ";
+
+	cin >> Size;
+
+	Info *employee = new Info[Size];
+
+	for (int i = 0; i < Size; i++)
+	{
+		cout << "\n\tEnter name of employee: ";
+		cin >> employee[i].name;
+		cout << "\n\tEnter surname of employee: ";
+		cin >> employee[i].surname;
+		cout << "\n\tEnter age of employee: ";
+		cin >> employee[i].age;
+		cout << "\n\tEnter discription about employee: ";
+		cin >> employee[i].discription;
+		cout << "\n\tEnter post of employee: ";
+		cin >> employee[i].post;
+		cout << "\n\tEnter number of work days of employee: ";
+		cin >> employee[i].workDays;
+		cout << "\n\tEnter weekends of employee: ";
+		cin >> employee[i].weekends;
+		cout << "\n\t============================================" << endl << endl;
+	}
+	ofstream file;
+
+	file.open("I.E.txt", ofstream::app);
+
+	for (int i = 0; i < Size; i++)
+	{
+		file << employee[i].SaveFile();
+		file.close();
+	}
+}
+
+void Menu()
 {
 	int choice = 0;
+	cout << "\n\t\t\t\t\t\t_______________________________________" << endl;
 	cout << "\n\t\t\t\t\t\t  Accounting of the enterprise (0.1)." << endl;
+	cout << "\n\t________________________________________________________________________________________________________" << endl;
 	cout << "\n\tTo fill information about employee, PRESS 1. " << "\n\n\tTo add new employee, PRESS 2. " << "\n\n\tTo delete an employee and info about him, PRESS 3. " << "\n\n\tTo see information about all employees, PRESS 4. " << "\n\n\tTo see information about direct employee, PRESS 5. " << "\n\n\tTo exit, PRESS 0. " << endl << endl;
+	cout << "\t________________________________________________________________________________________________________" << endl;
 	cout << "\n\tYour choice: ";
 	cin >> choice;
-	if (int choise = 4) {
+	if (int choice = 4) {
 		system("cls");
 		cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\tLoading...";
 		Sleep(1000);
 		system("cls");
-		Bill.ShowInfo();
-		cout << "\n\n\tTo move back press 1: ";
-		cin >> choice;
-		if (choice == 1) {
-			system("cls");
-			cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\tLoading...";
-			Sleep(1000);
-			system("cls");
-			Menu(Bill);
-		}
-		if (choice == 0) {
-			exit(0);
-		}
+		Info info;
+		info.ShowInfo();
+	}
+	if (int choice = 1) {
+
+		system("cls");
+		cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\tLoading...";
+		Sleep(1000);
+		system("cls");
+		FillInfo();
+
 	}
 }
 
-void LogIn(string login, string passwd, string login1, string passwd1, Employee Bill)
+void LogIn(string login, string passwd, string login1, string passwd1)
 {
 	cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\tTo enter the program, please, enter info below." << endl;
 	cout << "\n\t\t\t\t\t\tEnter your login: ";
@@ -57,12 +107,12 @@ void LogIn(string login, string passwd, string login1, string passwd1, Employee 
 	cin >> passwd1;
 
 	if (login1 == login && passwd1 == passwd) {
-		
+
 		system("cls");
 		cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\tAccess is alowed.";
 		Sleep(1000);
 		system("cls");
-		Menu(Bill);
+		Menu();
 	}
 	else
 	{
@@ -79,23 +129,13 @@ int main() {
 	string login1;
 	string passwd1;
 
-	Employee Bill;
-	Bill.name = "Bill";
-	Bill.surname = "Mc.Gonahan";
-	Bill.age = 20;
-	Bill.discription = "Nice guy with some nice skils!";
-	Bill.post = "software developer";
-	Bill.workDays = 25;
-	Bill.weekends = 10;
-	Bill.salary = 20000;
-
 	cout << endl;
 	cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\tWe preapering everything to start, please wait a moment..." << endl;
 	cout << "\n\t\t\t\t\t\tLoading..." << endl;
 	Sleep(1000);
 	system("cls");
 
-	LogIn(login, passwd, login1, passwd1, Bill);
+	LogIn(login, passwd, login1, passwd1);
 
 	system("pause");
 	return 0;
